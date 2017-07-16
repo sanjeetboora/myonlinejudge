46faf839a033820b2b123a64ea64c72e8e806a06
sanjeet <you@example.com>
2017-07-16 17:18:46 +0530

    onlinejudge compilation successful

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,16 +7,18 @@ class HomeController < ApplicationController
   		@names = @langs["languages"]["names"]
   		@codes = @langs["languages"]["codes"]
       # @ques=Question.find(params[:questions_id])
-      @ques=Question.new
+      @ques=Question.all
+
       # @testcases=@ques.testcases
 
   	end
 
   	def evaluate
-      @ques=Question.new
+
   		permitted = params.permit("source","lang","testcases","api_key","format")
   		response = HTTParty.post("http://api.hackerrank.com/checker/submission.json",:body => permitted);
-byebug
+      
+    end
 
-  	end
+
 end
