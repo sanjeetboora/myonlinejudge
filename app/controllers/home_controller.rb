e115805345b9469c4bc8567b90a72128a881aa54
sanjeet <you@example.com>
2017-07-16 16:29:41 +0530

    onlinejudge hckbefore testcases issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,13 +6,17 @@ class HomeController < ApplicationController
   		@langs = HTTParty.get('http://api.hackerrank.com/checker/languages.json')
   		@names = @langs["languages"]["names"]
   		@codes = @langs["languages"]["codes"]
+      # @ques=Question.find(params[:questions_id])
+      @ques=Question.new
+      # @testcases=@ques.testcases
+
   	end
 
   	def evaluate
-
+      @ques=Question.new
   		permitted = params.permit("source","lang","testcases","api_key","format")
   		response = HTTParty.post("http://api.hackerrank.com/checker/submission.json",:body => permitted);
-  		byebug
+byebug
 
   	end
 end
